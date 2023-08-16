3b1b79430cd69ee2bdc27b845963d7c4bdf61066
elboqueronpaco <elboqueronpaco@gmail.com>
2023-08-16 17:35:40 +0200

    validacion con zod

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const movies = require('./database/movies.json')
+const crypto = require('crypto')
+const { validateMovie } = require('./validator/movies')
 
 const app = express()
 app.disable('x-powered-by')
@@ -7,6 +10,38 @@ app.use(express.urlencoded({extended: false}))
 const port = process.env.PORT ?? 8000
 app.get('/', (req, res) =>{
     res.status(200).json({message: 'Mi pagina'})
+})
+app.get('/api/v1/movies', (req, res) => {
+    res.status(200).json(movies)
+})
+app.get('/api/v1/movies/:id', (req, res) => {
+    const {id} = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (!movie)
+        res.status(404).json({message: 'Movie not found'})
+    res.status(200).json(movie)
+   
+})
+app.get('/api/v1/movies', (req, res) => {
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(genreMovie => genreMovie.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+})
+app.post('/api/v1/movies', (req, res) => {
+    
+    const result = validateMovie(req.body)
+    if(result.error)
+        return res.status(400).json({error: JSON.parse(result.error.message)})
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+    movies.push(newMovie)
+    res.status(201).json(newMovie)
 })
 app.use((req, res) =>{
     res.status(404).send('<h2>Page Not Found</h2>')
